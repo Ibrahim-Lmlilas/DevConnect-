df9457863db7e03095dbe0506ad5495d5659dac4
brahim lmlilas <spedibra@gmail.com>
2025-02-26 22:00:57 +0100

    DevConnect

---
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
+            'skills' => ['nullable', 'string', 'max:255'],
+            'programming_languages' => ['nullable', 'string', 'max:255'],
+            'projects' => ['nullable', 'string', 'max:255'],
+            'certifications' => ['nullable', 'string', 'max:255'],
+            'github_url' => ['nullable', 'string', 'max:255'],
+            'industry' => ['nullable', 'string', 'max:255'],
+            'banner' => ['nullable', 'string', 'max:255'],
+            'bio' => ['nullable', 'string'],
+            'image' => ['nullable', 'image', 'max:2048'],
+        ];
+    }
+}
